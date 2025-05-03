8108c9811e293bbbdf4d6f69bc1be5306feb8b8a
pablo.martins <pablo.martins@gamersbank.com.br>
2025-05-03 12:54:27 -0300

    refactor: networking

--- src/modules/networking/main.tf
+++ src/modules/networking/main.tf
@@ -98,7 +98,7 @@ resource "aws_route_table" "rds_private_rt" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main[count.index].id
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
   tags = merge(
     var.tags,
